28656ec88952de214fdd27078cf8944a1c120479
Lerin17 <lerinowoade17@gmail.com>
2022-06-10 12:36:32 +0100

    slate setup #6

--- src/1Textarea/Utilts/textstyle/fontstyle.js
+++ src/1Textarea/Utilts/textstyle/fontstyle.js
@@ -6,41 +6,68 @@ import React from "react"
 
 
 const Leaf = props => {
-  // const [firstx, setfirstx] = React.useState(true);
 
-  // const incomingText= props.children.props.leaf.text.length
 
   
-
-  
-console.log(props.children.props)
+// console.log(props.children.props)
 
 const content = props.children.props.leaf
 
 
 const contentStyle = {
   fontWeight: content.bold ? 'bold' : 'normal',
-  fontStyle: content.italics? 'italic': 'normal'
+  fontStyle: content.italics? 'italic': 'normal',
 }
 
-  
+// const styleobjects = () => {
+//  styleobj = {
+//    isItalics : content.italics
+//  }
+//  console.log(styleobj)
+// }
+
+
+
     
      return (
-      <span onClick={()=>console.log(content.italics)}
+      <span onMouseEnter={()=>getstyle(content)}
       {...props.attributes}
       style={contentStyle}
     >
       {props.children}
     </span>
      )
-    
-    
+        
 }
 
 const DefaultElement = props => {
   return <p {...props.attributes} >{props.children}</p>
 }
 
+const getstyle  = (props) => {
+
+let styleobj 
+// console.log('eeee')
+if(props){
+  console.log(props)
+}
+
+if(props){
+  styleobj = {
+    isItalics: props.italics,
+    isBold: props.bold
+  }
+}
 
 
-export  {Leaf, DefaultElement}
+
+ return (
+  styleobj
+ ) 
+
+}
+
+
+
+
+export  {Leaf, DefaultElement, getstyle}
